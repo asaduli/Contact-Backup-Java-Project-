a4f01eedc7409fa1ee77ea41d7cec0d87f69d30f
adaduli <asadulislam.diu@gmail.com>
2018-02-05 23:15:53 +0600

    Home Page design for Contact Backup Application

---
+++ src/contactbackup/ContactBackup.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactbackup;
+
+/**
+ *
+ * @author Seyam
+ */
+public class ContactBackup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     HomePage hp=new HomePage();
+     hp.setVisible(true);
+    }
+    
+}
